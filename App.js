04a0df2b1f0d5aec7fddf3963a4f8ed76c3ca14b
ordegani <oradeganii@gmail.com>
2021-07-15 19:33:45 +0300

    changes

--- App.js
+++ App.js
@@ -1,25 +1,24 @@
-//backend only 
+//backend only
 const express = require("express");
 const mongoose = require("mongoose");
-const setupController = require('./controllers/setupControllers')
-const apicontroller = require("./controllers/apiControllers")
+const setupController = require("./controllers/setupControllers");
+const apicontroller = require("./controllers/apiControllers");
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-const uri = ''
+const uri = "";
 
-app.use(express.json())
+app.use(express.json());
 
-mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true  })
+mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
 
 app.get("/", (req, res) => {
   res.send("MOVIES APP!");
 });
 
-setupController(app)
-apicontroller(app)
+setupController(app);
+apicontroller(app);
 
 app.listen(port, () => console.log("app is running on port: " + port));
-
