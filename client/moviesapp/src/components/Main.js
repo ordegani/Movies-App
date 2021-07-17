d4f6e06280a3622bcb94baabce8e5062f2a3ef6d
ordegani <oradeganii@gmail.com>
2021-07-17 16:05:26 +0300

    changes

--- client/moviesapp/src/components/Main.js
+++ client/moviesapp/src/components/Main.js
@@ -7,11 +7,13 @@ export default main = () => {
   const apikey = `b8dd69ac`;
 
   const getMovies = async () => {
+    
     const response = await fetch(
-      `http://www.omdbapi.com/?t=free+willy&apikey=${apikey}`
-    );
+      `http://www.omdbapi.com/?t=free+willy&apikey=${apikey}`);
     const data = await response.json();
+    
     setMovies(data.hits);
+
     console.log(data.hits);
   };
 };
