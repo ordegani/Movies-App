7401eb1c1b126cec15d613b23d4b2fb3514a6786
ordegani <oradeganii@gmail.com>
2021-07-17 16:03:42 +0300

    changes

--- client/moviesapp/src/components/Main.js
+++ client/moviesapp/src/components/Main.js
@@ -1,15 +1,17 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 
-export default main = () =>{
-//set 
-const [movies, setMovies] = useState([]);
-//fetch 
-const apikey = `b8dd69ac`;
+export default main = () => {
+  //set
+  const [movies, setMovies] = useState([]);
+  //fetch
+  const apikey = `b8dd69ac`;
 
-const getMovies = async () => {
-    const response = await fetch(`http://www.omdbapi.com/?t=free+willy&apikey=${apikey}`);
+  const getMovies = async () => {
+    const response = await fetch(
+      `http://www.omdbapi.com/?t=free+willy&apikey=${apikey}`
+    );
     const data = await response.json();
     setMovies(data.hits);
-      console.log(data.hits);
+    console.log(data.hits);
   };
-}
+};
