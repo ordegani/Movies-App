ddc9d2b35ef4a439e490b54ca1fa352aabadfaf7
ordegani <oradeganii@gmail.com>
2021-07-18 16:23:44 +0300

    changes

--- client/moviesapp/src/components/Navbar.js
+++ client/moviesapp/src/components/Navbar.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import '../App.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
 
 function Navbar() {
-    return (
-        <div>
-            <ul className="navbarUl">   
-
-                <li><Link className="link" to="/">Home</Link></li>
-                
-            </ul>
-        </div>
-    )
+  return (
+    <div>
+      <ul className="navbarUl">
+        <li>
+          <Link className="link" to="/">
+            Home
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Navbar;
