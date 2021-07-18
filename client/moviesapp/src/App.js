11bb1d829a37ce4d2366ab3386222b5524f4217f
ordegani <oradeganii@gmail.com>
2021-07-18 16:21:35 +0300

    changes

--- client/moviesapp/src/App.js
+++ client/moviesapp/src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Main from "./components/Main.js";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
       <div className="container">
-        {/* <Navbar /> */}
+        <Navbar />
         <br />
         <Route exact path="/" component={Main} />
       </div>
---
+++ client/moviesapp/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import '../App.css';
+
+function Navbar() {
+    return (
+        <div>
+            <ul className="navbarUl">   
+
+                <li><Link className="link" to="/">Home</Link></li>
+                
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar;
