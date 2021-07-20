6a7971ed355ceddf9d7b4c69305aebde1b138d8a
ordegani <oradeganii@gmail.com>
2021-07-21 00:17:07 +0300

    changes

--- client/moviesapp/src/components/Home.js
+++ client/moviesapp/src/components/Home.js
@@ -7,27 +7,27 @@ export default function Home() {
     const getMovies = async () => {
         const ApiKey = `5a1afd222f50a8c3a063760ec102b675`;
         const response = await fetch(
-          `https://api.themoviedb.org/3/discover/movie?api_key=${ApiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_watch_monetization_types=flatrate&primary_release_year=2021&with_runtime.lte<1`
+          `https://api.themoviedb.org/3/trending/all/day?api_key=5a1afd222f50a8c3a063760ec102b675&total_pages=1`
         );
         const data = await response.json();
     
-        setmovies(movies.results);
+        setmovies(data.results);
       };
       console.log(movies);
     
       useEffect(() => {
         getMovies();
-      },[movies]);
+      });
 
     return (
         <div className="moviesContainer">hi
-  {/* {movies.results.map((movie, index) => (
+  {movies.map((movie, index, title) => (
           <Movie
             key={movie.index}
             id={movie.index}
- 
-          />          */}
-        {/* ))} */}
+ title = {movie.overview}
+          />         
+     ))}
         </div>
     )
 }
