ba815d2514c838d41149c7ebf2259b4f7d4712a1
ordegani <oradeganii@gmail.com>
2021-07-24 01:49:28 +0300

    changes

--- client/moviesapp/src/components/Movie.js
+++ client/moviesapp/src/components/Movie.js
@@ -4,9 +4,9 @@ import "../App.css";
 export default function Movie(props) {
   return (
     <div className="movieContainer">
-      <h4 className="movieTitle">Movie: {props.title}</h4>
+      <h4>Movie: {props.title}</h4>
       <img src={props.img} width="300" height="300" alt="img" />
-      <h5> realeased in: {props.release}</h5>
+      <h5> realeased in: {props.release? props.release:"no information found"}</h5>
       <h6>Rating: {props.rating}</h6>
       <p>{props.overview}</p>
     </div>
