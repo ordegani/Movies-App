ff9a5ee30307fc7c8d1823aef6dd8b922d21bec2
ordegani <oradeganii@gmail.com>
2021-08-11 16:43:53 +0300

    changes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,2 +1,8 @@
 const router = require("express").Router();
 const { User } = require("../models/userModel");
+
+router.route("/").get((req, res)=>{
+    User.find()
+    .then(users=>res.json(users))
+    .catch(err=>res.status(400).json("error:"+err));
+});
