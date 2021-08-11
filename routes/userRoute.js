db524687958222a6bc42e2f7f123ebc8af1cd2f7
ordegani <oradeganii@gmail.com>
2021-08-11 16:55:53 +0300

    changes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,3 +6,13 @@ router.route("/").get((req, res)=>{
     .then(users=>res.json(users))
     .catch(err=>res.status(400).json("error:"+err));
 });
+
+router.route("/add").post((req, res)=>{
+    const username = req.body.username;
+    const NewUser = new User({username});
+
+    NewUser.save()
+    .then (()=>res.json('added successfully'))
+    .carch(()=>res.status(400).json("error:"+err));
+});
+
