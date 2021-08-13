1c8af7ed488a61e32109fb06d445b2bd60d127ef
ordegani <oradeganii@gmail.com>
2021-08-13 23:23:28 +0300

    changes

--- client/moviesapp/src/App.js
+++ client/moviesapp/src/App.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import "./App.css"
-import SearchForFilm from "./components/SearchForFilm.js"
-import Navbar from "./components/Navbar"
-import Home from "./components/Home"
-import Saved from "./components/Saved"
-import Authentication from "./components/Authentication"
-import Login from "./components/Login.js"
+import "./App.css";
+import SearchForFilm from "./components/SearchForFilm.js";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Saved from "./components/Saved";
+import Authentication from "./components/Join";
+import Login from "./components/Login.js";
 function App() {
   const [save, setsave] = useState([]);
   return (
@@ -14,14 +14,18 @@ function App() {
       <div className="container">
         <Navbar />
         <br />
-        <Route exact path="/api/users/register" component={Authentication}></Route>
+        <Route
+          exact
+          path="/api/users/register"
+          component={Authentication}
+        ></Route>
         <Route exact path="/api/users/login" component={Login}></Route>
         <Route exact path="/search" component={SearchForFilm} />
         <Route path="/" exact>
-          <Home save={save} setsave={setsave}/>
+          <Home save={save} setsave={setsave} />
         </Route>
         <Route path="/saved" exact>
-          <Saved save={save} setsave={setsave}/>
+          <Saved save={save} setsave={setsave} />
         </Route>
       </div>
     </Router>
