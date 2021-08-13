46a2351542e0294df6ee097587c5730e321ea996
ordegani <oradeganii@gmail.com>
2021-08-14 00:05:13 +0300

    changes

--- client/moviesapp/src/components/Join.js
+++ client/moviesapp/src/components/Join.js
@@ -2,7 +2,7 @@ import { React, useState, useEffect } from "react";
 import axios from "axios";
 import "../App.css";
 
-export default function Authentication() {
+export default function Join() {
   let username;
   const saveNewUser = (username) => {
     axios
