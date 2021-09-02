fa97b587ed7d21a0f0342aa0b8b391e6322c0d3b
ordegani <oradeganii@gmail.com>
2021-09-03 00:10:59 +0300

    changes

--- client/moviesapp/src/components/Login.js
+++ client/moviesapp/src/components/Login.js
@@ -3,12 +3,12 @@ import { React, useState, useEffect } from "react";
 import "../App.css";
 
 export default function Login() {
-  const getUsers=()=>{
+  const getUsers = () => {
     axios
-  .get("http://localhost:5000/users/")
-  //TODO condition to check if username=username2?
-  .then((res) => console.log(res.data.username));
-}
+      .get("http://localhost:5000/users/")
+      //TODO condition to check if username=username2?
+      .then((res) => console.log(res.data.username));
+  };
   return (
     <div className="loginContainer">
       <h2>Welcome Back</h2>
