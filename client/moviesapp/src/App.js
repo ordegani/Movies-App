09e992844becc3593e17a9853f4bc40273b27d7b
ordegani <oradeganii@gmail.com>
2021-09-05 23:17:59 +0300

    changes

--- client/moviesapp/src/App.js
+++ client/moviesapp/src/App.js
@@ -7,6 +7,7 @@ import Home from "./components/Home";
 import Saved from "./components/Saved";
 import Join from "./components/Join";
 import Login from "./components/Login.js";
+
 function App() {
   //TODO - change from useState to call to the backend
   // const [save, setsave] = useState([]);
