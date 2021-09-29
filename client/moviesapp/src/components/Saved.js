b657f3ef6586742f458937006d273c86d9d2f614
ordegani <oradeganii@gmail.com>
2021-09-30 00:15:35 +0300

    changes

--- client/moviesapp/src/components/Saved.js
+++ client/moviesapp/src/components/Saved.js
@@ -31,7 +31,7 @@ export default function Saved({save, setsave}) {
   return (
     <div className="homeContainer">
       <button
-        className={save == [] ? "hidden" : "reset"}
+        className={save.length = 0 ? "hidden" : "reset"}
         //TODO axios.delet
         onClick={() => setsave([])}
       >
