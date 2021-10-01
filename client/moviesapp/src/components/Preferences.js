7bbf5f25dd78c23927001aba1189c8d90ed31eb5
ordegani <oradeganii@gmail.com>
2021-10-01 23:20:46 +0300

    changes

--- client/moviesapp/src/components/Preferences.js
+++ client/moviesapp/src/components/Preferences.js
@@ -1,2 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react"
 import "../App.css"
+
+import React from 'react'
+
+export default function Preferences() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
