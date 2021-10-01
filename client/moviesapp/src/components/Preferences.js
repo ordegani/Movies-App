657c9d31aac5713fd86508500e894867f891678a
ordegani <oradeganii@gmail.com>
2021-10-01 23:25:41 +0300

    changes

--- client/moviesapp/src/components/Preferences.js
+++ client/moviesapp/src/components/Preferences.js
@@ -6,8 +6,8 @@ import React from 'react'
 export default function Preferences() {
     return (
         <div>
-            <h2>Dark Mode</h2>
-            <h2>Quentin Trantino Mode</h2>
+            <h2>Tarantino (Dark Mode)</h2>
+            <h2>Almodovar (Day light)</h2>
         </div>
     )
 }
