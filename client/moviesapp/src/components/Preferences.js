283e5c1e496c9a3c0e32829a8c51658de5ce7471
ordegani <oradeganii@gmail.com>
2021-10-02 01:11:10 +0300

    changes

--- client/moviesapp/src/components/Preferences.js
+++ client/moviesapp/src/components/Preferences.js
@@ -1,14 +1,13 @@
-import React, { useEffect, useState } from "react"
-import "../App.css"
-import Movie from "./Movie"
+import React, { useEffect, useState } from "react";
+import "../App.css";
+import Movie from "./Movie";
 
 export default function Preferences() {
-    const modeChange = (e)=>{
-    }
-    return (
-        <div>
-            <button>Dark</button>
-            <button>Light</button>
-        </div>
-    )
+  const modeChange = (e) => {};
+  return (
+    <div>
+      <button>Dark</button>
+      <button>Light</button>
+    </div>
+  );
 }
