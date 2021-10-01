4a12e036ae304b5022832c24a40f4d3c78d43f3f
ordegani <oradeganii@gmail.com>
2021-10-02 01:23:39 +0300

    changes

--- client/moviesapp/src/components/Preferences.js
+++ client/moviesapp/src/components/Preferences.js
@@ -3,12 +3,15 @@ import "../App.css";
 import Movie from "./Movie";
 
 export default function Preferences() {
-    const [mode, setmode] = useState("")
-  const modeChange = (e) => {e => setmode(e.target.value)};
+    const [mode, setmode] = useState("");
+    const updateMode = (e) => {
+        setmode(e.target.value);
+      };
+
   return (
     <div>
-      <button>Dark</button>
-      <button>Light</button>
+      <button onClick={updateMode}>Dark</button>
+      <button nClick={updateMode}>Light</button>
     </div>
   );
 }
